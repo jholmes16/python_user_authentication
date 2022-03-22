5ee00b40bb8baac2792cafbc798e99fe081f35d0
jholmes16 <jholmesarea@gmail.com>
2022-03-22 15:22:47 -0400

    create login form

---
+++ myproject/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords must match!')])
+    pass_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Register!')
+
+    def validate_email(self, email):
+            if User.query.filter_by(email = self.email.data).first():
+                raise ValidationError('Your email has already been registered')
+
+    def validate_username(self, username):
+            if User.query.filter_by(username = self.username.data).first():
+                raise ValidationError('Username has been registered')
