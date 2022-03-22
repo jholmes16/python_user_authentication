1ca892683d0a853481192f20b45f55a3dda89188
jholmes16 <jholmesarea@gmail.com>
2022-03-22 16:48:22 -0400

    create base, welcome views and index, welcome logout routes

---
+++ app.py
@@ -0,0 +1,16 @@
+from myproject import app, db
+from flask import render_template, redirect, request, url_for, flash, abort
+from flask_login import login_user, login_required, logout_user
+from myproject.models import User
+from myproject.forms import LoginForm, RegistrationForm
+from werkzeug.security import generate_password_hash, check_password_hash
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/welcome')
+@login_required
+def welcome_user():
+    return render_template('welcome_user.html')
