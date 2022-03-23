ecc196f59076e050c4bd5b5d0ec9425adf27131e
jholmes16 <jholmesarea@gmail.com>
2022-03-23 14:20:45 -0400

    connect forms to app file

--- myproject/models.py
+++ myproject/models.py
@@ -20,5 +20,5 @@ class User(db.Model, UserMixin):
         self.username = username
         self.password_hash = generate_password_hash(password)
 
-    def check_password_hash(self, password):
+    def check_password(self, password):
         return check_password_hash(self.password_hash, password)
