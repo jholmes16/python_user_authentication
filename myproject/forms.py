9e6417f4b06984539b7a07199918bd21cd8e1eb7
jholmes16 <jholmesarea@gmail.com>
2022-03-23 14:59:50 -0400

    add templates

--- myproject/forms.py
+++ myproject/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, ValidationError
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired,Email,EqualTo
+from wtforms import ValidationError
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -8,16 +9,18 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Login")
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(),Email()])
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords must match!')])
-    pass_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords Must Match!')])
+    pass_confirm = PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField('Register!')
 
-    def validate_email(self, email):
-            if User.query.filter_by(email = self.email.data).first():
-                raise ValidationError('Your email has already been registered')
+    def check_email(self, field):
+        # Check if not None for that user email!
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Your email has been registered already!')
 
-    def validate_username(self, username):
-            if User.query.filter_by(username = self.username.data).first():
-                raise ValidationError('Username has been registered')
+    def check_username(self, field):
+        # Check if not None for that username!
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Sorry, that username is taken!')
